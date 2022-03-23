37982b931f7b2e4fb9a8b47bf6f9ca42d35ccb6a
Tilen Komel <tilen.komel10@gmail.com>
2022-03-23 23:23:18 +0100

    Deployment process modified

--- docker/grafana-monitoring/deploy.sh
+++ docker/grafana-monitoring/deploy.sh
@@ -13,14 +13,17 @@ mkdir /appdata/grafana/etc-grafana
 mkdir /appdata/grafana/var-lib-grafana
 mkdir /appdata/influxdb
 
+chmod -R 776 /appdata/grafana
+
 # Get Telegraf configuration file
 wget -nc -O /appdata/telegraf/telegraf.conf https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/telegraf.conf
 
 # Get Grafana configuration file
 wget -nc -O /appdata/grafana/etc-grafana/grafana.ini https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/grafana.ini
 
-cp -u nginx.conf /etc/nginx/sites-available/grafana-monitoring
+cp -n nginx.conf /etc/nginx/sites-available/grafana-monitoring
 ln -s /etc/nginx/sites-available/grafana-monitoring /etc/nginx/sites-enabled/grafana-monitoring
-nginx -t && service nginx restart && echo "Nginx OK"
 
-echo "Now create 'docker-compose.yaml' file from 'docker-compose.sample.yaml' and then docker-compose up -d"
+echo "Edit 'server_name' in Nginx configuration: 'sudo nano /etc/nginx/sites-enabled/grafana-monitoring'."
+echo "When you done do 'sudo nginx -t' if comand ends with success run 'sudo service nginx restart'."
+echo "Then create 'docker-compose.yaml' file from 'docker-compose.sample.yaml' fill all ENV and then run 'docker-compose up -d'"
