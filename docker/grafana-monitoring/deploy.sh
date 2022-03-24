b3f90e317b7034210f32dd1122f9b33c2adb934b
Tilen Komel <tilen.komel10@gmail.com>
2022-03-24 02:27:31 +0100

    Modifications

--- docker/grafana-monitoring/deploy.sh
+++ docker/grafana-monitoring/deploy.sh
@@ -6,26 +6,76 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Create needed folders
-mkdir /appdata
-mkdir /appdata/telegraf
-mkdir /appdata/grafana
-mkdir /appdata/grafana/etc-grafana
-mkdir /appdata/grafana/var-lib-grafana
-mkdir /appdata/influxdb
+mkdir -p /appdata/telegraf && echo "Telegraf folder created successfully!"
 
-chmod -R 776 /appdata/grafana
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+mkdir -p /appdata/grafana/etc-grafana && echo "Grafana folder 1 created successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+mkdir -p /appdata/grafana/var-lib-grafana && echo "Grafana folder 2 created successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+mkdir -p /appdata/influxdb && echo "InfluxDB folder created successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+chmod -R 776 /appdata/grafana && echo "Rights successfully altered to Grafana folder!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
 
 # Get Telegraf configuration file
-wget -nc -O /appdata/telegraf/telegraf.conf https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/telegraf.conf
+wget -nc -O -q /appdata/telegraf/telegraf.conf https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/telegraf.conf && echo "Configuration file for Grafana downloaded successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
 
 # Get Grafana configuration file
-wget -nc -O /appdata/grafana/etc-grafana/grafana.ini https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/grafana.ini
+wget -nc -O -q /appdata/grafana/etc-grafana/grafana.ini https://raw.githubusercontent.com/KomelT/boilerplates/master/docker/grafana-monitoring/conf/grafana.ini && echo "Configuration file for Telegraf downloaded successfully!"
 
-cp -n nginx.conf /etc/nginx/sites-available/grafana-monitoring
-ln -s /etc/nginx/sites-available/grafana-monitoring /etc/nginx/sites-enabled/grafana-monitoring
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
 
-echo ""
-echo ""
-echo "Edit 'server_name' in Nginx configuration: 'sudo nano /etc/nginx/sites-enabled/grafana-monitoring'."
-echo "When you done do 'sudo nginx -t' if comand ends with success run 'sudo service nginx restart'."
-echo "Then create 'docker-compose.yaml' file from 'docker-compose.sample.yaml' fill all ENV and then run 'docker-compose up -d'"
+cp -n nginx.conf /etc/nginx/sites-available/grafana-monitoring && echo "Nginx config copied successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+ln -s /etc/nginx/sites-available/grafana-monitoring /etc/nginx/sites-enabled/grafana-monitoring && echo "Symbolic link created successfully!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+nginx -t && service nginx restart && echo "Nginx config is OK!"
+
+if [ $? -ne 0 ]; then
+    echo "Exitting because of error!"
+    exit 1
+fi
+
+docker-compose up -d
